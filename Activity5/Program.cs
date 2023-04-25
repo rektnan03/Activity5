e7af6f96e075490c555426eca00ef638d91ef3f6
Administrator <Administrator@DESKTOP-ET8QFN2>
2023-04-25 16:51:10 -0700

    Add project files.

---
+++ Activity5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Activity5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lines;
+
+            StreamReader sR = new StreamReader("C:\\Users\\Administrator\\source\\repos\\hehe.txt");
+
+            lines = sR.ReadLine();
+
+            while (lines != null)
+            {
+                Console.WriteLine(lines);
+                lines = sR.ReadLine();
+            }
+
+            sR.Close();
+
+            StreamWriter sW = new StreamWriter("C:\\Users\\Administrator\\source\\repos\\hehe.txt");
+
+            sW.WriteLine("Activity ni Inan");
+            sW.WriteLine("hehe");
+
+            sW.Close();
+
+        }
+    }
+}
